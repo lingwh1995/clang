920fa48f16da1dd96ad557efba93ef08a4c1ad21
think <lingwh.walkman@hotmail.com>
2025-06-10 14:32:32 +0800

    修改项目结构

---
+++ 100_测试程序专用/src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * 从尾到头在一个字符串中查找给定子串的第一个匹配之处
+ */ 
+char* Strstr(const char* str, const char* substr)
+{
+    if(NULL == str || NULL == substr)
+    {
+        return NULL;
+    }
+	int m = strlen(str);
+	int n = strlen(substr);
+	for(int i=0; i < m - n + 1; i++)
+	{
+		int k = i;
+		int j = 0;
+		while(str[k] == substr[j] && j < n)
+		{
+			k++;
+			j++;
+		}
+		if(j == n)
+		{
+			return (char*)str + i;
+		}
+	}
+	return NULL;
+}
+
+int main()
+{
+	const char* str = "abccabcabbc";
+	const char* substr = "abc";
+	char* result = Strstr(str,substr);
+	printf("result = %s\n", result);
+	return 0;
+}
