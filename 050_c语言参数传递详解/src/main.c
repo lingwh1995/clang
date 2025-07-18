9511adb5985ede3f0fd793d41b3de19d4fa932b5
lingwh <lingwh.walkman@homail.com>
2025-07-18 22:43:21 +0800

    修改项目结构

--- 050_c语言参数传递详解/src/main.c
+++ 050_c语言参数传递详解/src/main.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 
-void fun(int a)
+void fun1()
 {
-    printf("a = %d\n", a);
+    int a = 10;
+    int arr[10] = { 12, 23, 34, 45, 56 };
+    double dx = 12.23;
+    char* cp = NULL;
+    int* s = &a;
 
+    void* vp = NULL;
+    vp = &a;
+    vp = &arr;
+    vp = arr;
+    vp = &dx;
+
+    vp = cp;
+    vp = &s; // 二级指针
 }
+
+void fun2()
+{
+    int a = 10;
+    int* ip = NULL;
+    char ch = 'a';
+    char* cp = NULL;
+    void* vp = &a;
+
+    //*vp = 100;  // void* 不能解引用
+    //vp = vp + 1;  // vp不能加1
+
+    ip = (int*)vp;
+    vp = &ch;
+
+    cp = (char*)vp;
+}
+
 int main()
 {
-    fun(10);
-    printf("hello world!\n");
+    fun1();
+    fun2();
     return 0;
 }
