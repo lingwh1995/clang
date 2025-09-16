53101a8078c23b8883c9f4b49637f8c47896daa8
lingwh <lingwh.walkman@homail.com>
2025-09-16 23:17:27 +0800

    修改项目结构

--- 053_打字母游戏/src/main.c
+++ 053_打字母游戏/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <windows.h>
 
 #define ROWSIZE 20
 #define COLSIZE 35
@@ -32,23 +34,45 @@ void init_grid(GridArray ga)
 /**
  * 打印网格
  */
-void show_grid(GridArray ga)
+void show_grid(GridArray ga, struct LetterNode* px, int n)
 {
+	if(NULL == px)
+	{
+		return;
+	}
+	//system("cls");
+    printf("------------------\n");
+	init_grid(ga);
+	for(int i = 0; i < n; i++)
+	{
+		ga[px[i].row][px[i].col] = px[i].ch;
+	}
 	for(int i=0; i < ROWSIZE; i++)
 	{
-		for(int j = 0; j < COLSIZE; j++)
-		{
-			printf("%c", ga[i][j]);
-		}
-		printf("\n");
+//        for(int j = 0; j < COLSIZE + 1; j++)
+//        {
+//            printf("%c", ga[i][j]);
+//        }
+//        printf("\n");
+		printf("%s \n", ga[i]);
 	}
 }
 
 int main()
 {
 	GridArray ga;
-	//struct LetterNode x[LETSIZE] = { 0 };
-	init_grid(ga);
-	show_grid(ga);
+	struct LetterNode x[LETSIZE] = { 0 };
+
+	x[0].row = 1;
+	x[0].col = 5;
+	x[0].ch = '0';
+
+	for(int i = 0; i < 5; i++)
+	{
+		show_grid(ga, x, LETSIZE);
+		x[0].row++;
+		Sleep(1000);
+	}
+    //show_grid(ga, x, LETSIZE);
 	return 0;
 }
