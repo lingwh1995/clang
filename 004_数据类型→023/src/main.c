f1fb76e9769e5fa02a3754224bf250fce4ca0677
lingwh <lingwh.walkman@homail.com>
2025-09-21 21:13:14 +0800

    修改项目结构

--- 004_数据类型→023/src/main.c
+++ 004_数据类型→023/src/main.c
@@ -19,6 +19,8 @@
  *	   	 指针(Pointer)：指向特定类型数据的指针，用于存储变量的内存地址，如int *p表示指向整数的指针。
  *	   4.空类型
  *	   	 void‌：表示没有类型，通常用于函数返回值、函数参数或通用指针。
+ *	   5.数据类型转换
+ *	   	 从小范围数据类型转换为大范围数据类型，符号位扩展取决于原数据类型，而不是目标数据类型
  */
 
 
@@ -69,12 +71,12 @@ void print_color(enum Color color)
     }
 }
 
-
-int main()
+/**
+ * 不同数据类型占据的空间大小
+ */
+void data_type_size()
 {
-	/**
-	 * 不同数据类型占据的空间大小
-	 */
+
 	// short就是short int
     printf("short类型所占字节大小: %d\n", sizeof(short));
     printf("int类型所占字节大小: %d\n", sizeof(int));
@@ -93,11 +95,13 @@ int main()
     // 使用bool类型,C99中要引用 stdbool.h
     printf("bool类型所占字节大小: %d\n", sizeof(bool));
     printf("-------------------------------------\n");
+}
 
-
-    /**
-     * 定义不同的数据类型的数据
-     */
+/**
+ * 定义基本的数据类型
+ */
+void definition_basic_data_type()
+{
     // short类型
     short s_signed = -100;
     unsigned short s_unsigned = 100;
@@ -151,8 +155,13 @@ int main()
     char t = '\'';
     printf("%c\n", t);
     printf("-------------------------------------\n");
+}
 
-
+/**
+ * 定义数组数据类型
+ */
+void definition_array_data_type()
+{
     // 数组(Array)
     int nums[] = { 10, 20, 30 };
     int i;
@@ -163,8 +172,13 @@ int main()
     }
     printf("\n");
     printf("-------------------------------------\n");
+}
 
-
+/**
+ * 定义结构体数据类型
+ */
+void definition_struct_data_type()
+{
     // 结构体(Struct)
     struct Student alice = { 1,"Alice",18 };
     struct Student bob = { 2,"Bob",23 };
@@ -173,26 +187,36 @@ int main()
     		bob,
     		{ 3, "Charile", 29 }
     };
-    size = sizeof(student_arr)/sizeof(student_arr[0]);
-    for(i = 0; i < size; i++)
+    int size = sizeof(student_arr)/sizeof(student_arr[0]);
+    for(int i = 0; i < size; i++)
     {
     	printf("学生编号：%d,学生姓名：%s,学生年龄：%d\n", student_arr[i].id, student_arr[i].name, student_arr[i].age);
     }
     printf("-------------------------------------\n");
+}
 
-
+/**
+ * 定义枚举数据类型
+ */
+void definition_enum_data_type()
+{
     // 枚举(Enum)
     enum Color color = RED;
     printf("%d\n", color);
     print_color(color);
     // 注意：枚举可以像整型数组一样使用
-    for(i=RED; i<=BLUE; i++)
+    for(int i=RED; i <= BLUE; i++)
     {
     	printf("%d\n", i);
     }
     printf("-------------------------------------\n");
+}
 
-
+/**
+ * 定义指针数据类型
+ */
+void definition_pointer_data_type()
+{
     int a = 100;
     // 指针
     int* p = &a;
@@ -201,5 +225,149 @@ int main()
     // 数组的地址
     printf("数组的地址 = %p\n", &arr);
     printf("-------------------------------------\n");
+}
+
+
+/**
+ * 数据类型转换
+ */
+void data_type_conversion()
+{
+	int i = 0x12345678;
+	// 显式转换
+//	short s = (short)i;
+//	char c = (char)i;
+	// 隐式转换
+	short s = i;
+	char c = i;
+	printf("i = %x, s = %x, c = %x\n", i, s ,c);
+}
+
+/**
+ * 隐式类型转换案例一
+ */
+void implicit_data_type_conversion_1()
+{
+	// char a = -5         的二进制 => 原码 1000 0101 => 反码 1111 1010 => 补码 1111 1011 => int a（负数左边补1） => 1111 1111 1111 1111 1111 0101
+	char a = -5;
+	// unsigned int b = 10 的二进制 => 原码 0000 1010 => 反码 0111 0101 => 补码 0111 0111 => int b（正数左边补0） => 0000 0000 0000 0000 0000 1010
+	unsigned int b = 10;
+	if(a > b)
+	{
+		//printf("%x > %x \n", a, b);
+		printf("%d > %d \n", a, b);
+	}
+	else
+	{
+		//printf("%x < %x \n", a, b);
+		printf("%d < %d \n", a, b);
+	}
+}
+
+
+/**
+ * 隐式类型转换案例二
+ */
+void implicit_data_type_conversion_2()
+{
+	/**
+	 * char a = 100 的二进制 => 原码 0110 0100 => 反码 0110 0100 => 补码   0110 0100
+	 * 																	  +
+	 * char b = 200 的二进制 => 原码 1100 1000 => 反码 1100 1000 => 补码   1100 1000
+	 *
+	 * 运算结果  c 												=> 补码 1 0010 1100 => 截取最低位1个字节 => 0010 1100 => 十进制的44
+	 *
+	 * char a = 100 的二进制 => 补码 0110 0100 => int a 的二进制补码  0000 0000 0000 0000 0000 0000 0110 0100
+	 * 															     +
+	 * char b = 200 的二进制 => 补码 1100 1000 => int b 的二进制补码  1111 1111 1111 1111 1111 1111 1100 1000
+	 *
+	 * 运算结果  a + b 		 		 		  => int c 的二进制补码 10000 0000 0000 0000 0000 0000 0010 1100  => 十进制的44
+	 */
+	char a = 100;
+	char b = 200;
+	char c = a + b;
+	printf("c = %d, a + b = %d\n", c, a + b);
+
+	/**
+	 * unsigned char x = 100 的二进制 => 原码 0110 0100 => 反码 0110 0100 => 补码   0110 0100
+	 * 																	 	       +
+	 * unsigned char y = 200 的二进制 => 原码 1100 1000 => 反码 1100 1000 => 补码   1100 1000
+	 *
+	 * 运算结果												             => 补码 1 0010 1100 => 截取最低位1个字节 => 0010 1100 => 十进制的44
+	 *
+	 * unsigned char x = 100 的二进制 => 补码 0110 0100 => int x 的二进制补码 0000 0000 0000 0000 0000 0000 0110 0100
+	 * 															             +
+	 * unsigned char x = 200 的二进制 => 补码 1100 1000 => int x 的二进制补码 0000 0000 0000 0000 0000 0000 1100 1000
+	 *
+	 * 运算结果  x + y 		 		 		  	       => int z 的二进制补码 0000 0000 0000 0000 0000 0001 0010 1100  => 十进制的300
+	 */
+	unsigned char x = 100;
+	unsigned char y = 200;
+	unsigned char z = x + y;
+	printf("z = %d, x + y = %d\n", z, x + y);
+}
+
+
+/**
+ * 隐式类型转换案例三
+ */
+void implicit_data_type_conversion_3()
+{
+	char c = 128;
+	unsigned char uc = 128;
+	unsigned short us = 0;
+	/**
+	 * 			 char c => 1000 0000 => short  c   1111 1111 1000 0000
+	 *       			   						   +
+	 * unsigned char uc => 1000 0000 => short uc   0000 0000 1000 0000
+	 * unsigned short us  			 =>			 1 0000 0000 0000 0000 => 截最取低位2个字节 => 0000 0000 0000 0000 => 十六进制 0
+	 */
+	us = c + uc;
+	printf("us = %x\n", us);
+
+
+	/**
+	 *           char c => 1000 0000 => unsigned char  c => 1000 0000 => short  c  0000 0000 1000 0000
+	 *       			                          						           +
+	 * unsigned char uc => 1000 0000 => unsigned char uc => 1000 0000 => short uc  0000 0000 1000 0000
+	 * unsigned short us  						                      =>		   0000 0001 0000 0000 => 截最取低位2个字节 => 0000 0001 0000 0000 => 十六进制 100
+	 */
+	us = (unsigned char)c + uc;
+	printf("us = %x\n", us);
+
+
+	/**
+	 * 		    char  c => 1000 0000 => char  c => 1000 0000 => short  c   1111 1111 1000 0000
+	 *       			                           						   +
+	 * unsigned char uc => 1000 0000 => char uc => 1000 0000 => short uc   1111 1111 1000 0000
+	 * unsigned short us  			 						 =>			 1 1111 1111 0000 0000 => 截最取低位2个字节 => 1111 1111 0000 0000 => 十六进制 ff00
+	 */
+	us = c + (char)uc;
+	printf("us = %x\n", us);
+
+
+	/**
+	 * 			char  c => 1000 0000 => unsigned short c => 1111 1111 1000 0000 => unsigned short  c   1111 1111 1000 0000
+	 *       			                          		    +
+	 * unsigned char uc => 1000 0000 => unsigned char uc => 1000 0000 		    => unsigned short uc   0000 0000 1000 0000
+	 * unsigned short us  			 											=>		             1 0000 0000 0000 0000 => 截最取低位2个字节 => 0000 0000 0000 0000 => 十六进制 100
+	 */
+	// 特别注意： 符号位的扩展取决于原数据类型而不是目标数据类型，如这里将 char(原数据类型) => unsigned short(目标数据类型)，符号位的扩展取决于 原数据类型char
+	us = (unsigned short)c + uc;
+	printf("us = %x\n", us);
+}
+
+int main()
+{
+	data_type_size();
+	definition_basic_data_type();
+	definition_array_data_type();
+	definition_struct_data_type();
+	definition_enum_data_type();
+	definition_pointer_data_type();
+    data_type_conversion();
+    implicit_data_type_conversion_1();
+    implicit_data_type_conversion_2();
+    implicit_data_type_conversion_3();
     return 0;
 }
