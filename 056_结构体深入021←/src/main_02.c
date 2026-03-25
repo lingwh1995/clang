af3512ca038222894fb2d74ef854cc0d50130e0c
lingwh <lingwh.walkman@homail.com>
2026-03-25 23:36:03 +0800

    修改项目结构

--- 056_结构体深入021←/src/main_02.c
+++ 056_结构体深入021←/src/main_02.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
 /**
- * 链表节点结构体（
+ * 链表节点结构体
  */
 typedef struct node {
     int data;
     struct node *next;
-} node, *linked_list;
+} node;
 
-/**
- * @brief 打印链表
- * @param head 链表头节点指针
- */
-void print_linked_list(const linked_list head) {
+// 推荐单独链表指针别名
+typedef node *linked_list;
+
+void print_linked_list(const node *head) {
     if (head == NULL) {
         printf("链表为空\n");
         return;
     }
+
     /**
      * 1. 不推荐直接用 head，因为会丢失头指针位置
      * 2. 遍历指针的类型，必须和链表头指针的类型完全一致！
-     *    推荐 linked_list current = head; 而非 node *current = head;
      */
-    linked_list current = head;
+    const node *current = head;
     while (current != NULL) {
         printf("%d ", current->data);
         current = current->next;
@@ -30,7 +29,7 @@ void print_linked_list(const linked_list head) {
     printf("\n");
 }
 
-int main(void) {
+int main() {
     // 静态创建链表节点
     node node_1 = {1, NULL};
     node node_2 = {2, NULL};
