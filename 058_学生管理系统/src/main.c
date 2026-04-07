5e5e6c11507f9fb1ee16e7c8f1d6c8a48a0ddf5d
lingwh <lingwh.walkman@hotmail.com>
2026-04-07 23:24:03 +0800

    修改项目结构

--- 058_学生管理系统/src/main.c
+++ 058_学生管理系统/src/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+#define STUSIZE 100
+#define SCSIZE 3
+
+struct student_s
+{
+	int id;
+	char name[10];
+	char sex[4];
+	int age;
+	float score[SCSIZE];
+	float sum;
+	float ave;
+};
+
+struct student_s data[STUSIZE];
+
 int main()
 {
 
