1fa1760d057cdebdafef48dbfeafc3df31df82fb
think <lingwh.work@hotmail.com>
2026-05-07 16:23:41 +0800

    修改项目结构

--- 001-c-tutorial/01-基础入门/10_字符串和字符数组/src/main_01.c
+++ 001-c-tutorial/01-基础入门/10_字符串和字符数组/src/main_01.c
@@ -5,15 +5,17 @@
 #endif
 int main()
 {
+    // 使用续行符把多行拼接在一起
     char stra[] = { "this is a long string \
                         that is used \
                             as an example" };
+    // 不使用续行符把多行拼接在一起
     char strb[] = { "this is a long string"
                         "that is used"
                             "as an example" };
     printf("stra = %s \n", stra);
     printf("strb = %s \n", strb);
-    printf("strlen(stra) = %d \n", strlen(stra));
-    printf("strlen(strb) = %d \n", strlen(strb));
+    printf("strlen(stra) = %llu \n", strlen(stra));
+    printf("strlen(strb) = %llu \n", strlen(strb));
     return 0;
 }
--- 001-c-tutorial/20_预编译指令/01_预编译指令之宏定义/src/main_02.c
+++ 001-c-tutorial/20_预编译指令/01_预编译指令之宏定义/src/main_02.c
@@ -5,10 +5,20 @@
 #endif
 int main()
 {
-    char str[] = { "hello "ERROR_MSG };
-    printf("%s \n", str);
+    // 拼接字符串和宏值错误写法：这种写法编译器无法正确解析
+    //char stra[] = { "hello "ERROR_MSG };
+    // 拼接字符串和宏值正确写法：C语言支持相邻字符串字面量的自动连接
+    char stra[] = "hello " ERROR_MSG;
+    printf("%s \n", stra);
+
+    // C语言支持两个字符串的自动连接
+    char strb[] = "hello " "world!";
+    printf("%s \n", strb);
+
+    // 使用 %s 形式输出宏值
     printf("%s \n", ERROR_MSG);
-    // 特别注意： 宏定义的字符串可以不使用%s输出，直接就输出
+
+    // 特别注意： 宏值可以不使用 %s，直接就输出
     printf(ERROR_MSG);
     return 0;
 }
