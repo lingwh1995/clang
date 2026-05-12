4c764ad63c2782da722a695fa825d847187ea6c7
think <lingwh.work@hotmail.com>
2026-05-12 18:17:21 +0800

    修改项目结构

--- 001-c-tutorial/20_预编译指令/01_预编译指令之宏定义/src/main_07.c
+++ 001-c-tutorial/20_预编译指令/01_预编译指令之宏定义/src/main_07.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 // 使用 宏定义 + calloc 实现 简化calloc
@@ -6,17 +5,17 @@
 #define malloc void // 如果要禁用系统库函数，只使用宏定义
 #undef malloc   // 如果要再次使用系统库函数，则需要释放掉宏定义
 #if 0
-#endif
 int main()
 {
     int *ip_calloc = (int*)calloc(10, sizeof(int));
-    // 使用 malloc
+    // 使用系统库函数 malloc
     int *ip_malloc = (int *)malloc(sizeof(int) * 10);
     int *ip_CALLOC = CALLOC(10, int);
 
     char *cp_calloc = (char*)calloc(10, sizeof(char));
-    // 使用 malloc
+    // 使用系统库函数 malloc
     char *cp_malloc = (char *)malloc(sizeof(char) * 10);
     char *cp_CALLOC = CALLOC(10, char);
     return 0;
 }
+#endif
--- 001-c-tutorial/20_预编译指令/01_预编译指令之宏定义/src/main_08.c
+++ 001-c-tutorial/20_预编译指令/01_预编译指令之宏定义/src/main_08.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void swap_int(int *pa, int *pb)
 {
@@ -15,21 +14,57 @@ void swap_double(double *pa, double *pb)
     *pb = tmp;
 }
 
+/**
+ * 注意事项
+ *
+ * 1. 这里不传递地址，因为替换后的代码在一个代码块中，而不是在另一个函数中
+ * 2. 使用异或运算不传递类型也能交换，不过仅适用于整数
+ *    #define SWAP_XOR(a, b)  { (a) ^= (b); (b) ^= (a); (a) ^= (b); }
+ * 3. 使用typeof或__typeof__关键字，可以实现自动推断数据类型，可以实现不传递类型也能交换
+ *    #define SWAP_TYPEOF(a, b)  { typeof(a) tmp = a; a = b; b = tmp; }
+ *    #define SWAP_TYPEOF(a, b)  { __typeof__(a) tmp = a; a = b; b = tmp; }
+ *
+ * typeof发展历史
+ *
+ * 1. C89/C99/C11/C17 中 typeof 都是编译器扩展
+ * 2. C23 标准化了 typeof，但 C23 刚刚发布（2023年），普及还需要时间
+ */
+
+// 通用的交换函数（必须传递数据类型）
 #define SWAP(a, b, type)  { type tmp = a; a = b; b = tmp; }
+// 不传递数据类型完成交换，但是只能用于整数
+#define SWAP_XOR(a, b)  { (a) ^= (b); (b) ^= (a); (a) ^= (b); }
+// 不传递数据类型完成交换，需要依赖编译器
+#ifdef _MSC_VER // _MSC_VER 是 MSVC 编译器预定义的宏
+#define SWAP_TYPEOF(a, b)  { __typeof__(a) tmp = a; a = b; b = tmp; } // Microsoft 编译器
+#else
+#define SWAP_TYPEOF(a, b)  { typeof(a) tmp = a; a = b; b = tmp; } // GCC/Clang 编译器
+#endif
 
 #if 0
+#endif
 int main()
 {
     int a = 10, b = 20;
+    // 传统方式交换
     //swap_int(&a, &b);
-    SWAP(a, b , int);
-    // 预处理后展开为
-    //{ int tmp = x; x = y; y = tmp; }
 
+    // 宏方式交换（需要传递数据类型）
+    //SWAP(a, b , int);  // 预处理后展开为 { int tmp = a; a = b; b = tmp; }
+
+    // 宏方式交换（不用传递数据类型）
+    SWAP_TYPEOF(a, b);
     printf("a = %d, b = %d\n", a, b);
+
     double x = 10.10, y = 20.20;
-    swap_double(&x, &y);
-    printf("x = %d, y = %d\n", x, y);
+    // 传统方式交换
+    //swap_double(&x, &y);
+
+    // 宏方式交换（需要传递数据类型）
+    //SWAP(x, y , double); // 预处理后展开为 { double tmp = x; x = y; y = tmp; }
+
+    // 宏方式交换（不用传递数据类型）
+    SWAP_TYPEOF(x, y);
+    printf("x = %.2f, y = %.2f\n", x, y);
     return 0;
 }
-#endif
